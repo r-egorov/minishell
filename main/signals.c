d51f09395c0896cd1aefded2d6185e353b3e1b74
Chansey Isis <cisis@at-q5.msk.21-school.ru>
2021-05-13 12:29:41 +0300

    SIGNALS: Ignore SIGTERM ( from the term)

--- main/signals.c
+++ main/signals.c
@@ -6,7 +6,7 @@
 /*   By: lelderbe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/20 10:46:18 by lelderbe          #+#    #+#             */
-/*   Updated: 2021/04/20 14:44:33 by lelderbe         ###   ########.fr       */
+/*   Updated: 2021/05/13 12:27:40 by cisis            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,12 @@ void	restore_child_sig()
 {
 	signal(SIGINT, SIG_DFL);
 	signal(SIGQUIT, SIG_DFL);
+	signal(SIGTERM, SIG_DFL);
 }
 
 void	ignore_parent_sig()
 {
 	signal(SIGINT, SIG_IGN);
 	signal(SIGQUIT, SIG_IGN);
+	signal(SIGTERM, SIG_IGN);
 }
